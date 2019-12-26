96eadf489125eaffbff9d657d9684bc1907ffcbf
bipin <bipin@pop-os.localdomain>
2019-12-26 15:28:37 +0545

    Revive the failed attempt to successful atemt.

--- get_viz.py
+++ get_viz.py
@@ -170,29 +170,6 @@ def process_video(vid_location, model_type, processed_vid_location):
         counter += 1
         print(parts['left_palm'], parts['right_palm'])
         try:
-            # Failed attempt at finding the angular movement
-            # if (parts['right_shoulder'][0] > parts['left_shoulder'][0]):
-            #     # shoulder_angle = angle_between_points(parts['right_shoulder'], parts['left_shoulder'], (0, parts['left_shoulder'][1])) + 90
-            #     if parts['left_palm'][0] is not None and (parts['left_palm'][0]<parts['right_shoulder'][0] and parts['left_palm'][1]<parts['right_shoulder'][1]):
-            #         print("yes")
-            #         shoulder_angle = angle_between_points(parts['left_shoulder'], parts['right_shoulder'], (0, parts['right_shoulder'][1])) - 90
-            #     elif parts['right_palm'][0] is not None and (parts['right_palm'][0]<parts['right_shoulder'][0] and parts['right_palm'][1]<parts['right_shoulder'][1]):
-            #         print("yes_1")
-            #         shoulder_angle = angle_between_points(parts['left_shoulder'], parts['right_shoulder'], (0, parts['right_shoulder'][1])) - 90
-            #     elif parts['left_palm'][0] is None and parts['right_palm'][0] is None:
-            #         print("None")
-            #         shoulder_angle = None
-            #     else:
-            #         print('latest')
-            #         shoulder_angle = angle_between_points(parts['right_shoulder'], parts['left_shoulder'], (0, parts['left_shoulder'][1])) + 90
-            # else:
-            #     print('no')
-            shoulder_angle = angle_between_points(parts['right_shoulder'], parts['left_shoulder'], (0, parts['left_shoulder'][1])) + 90
-        except:
-            print('exp')
-            shoulder_angle = None
-        print(shoulder_angle)
-        try:
             if parts['right_hip'][0] < parts['left_hip'][0]:
                 hip_angle = angle_between_points(parts['left_hip'], parts['right_hip'], (0, parts['right_hip'][1])) - 90
             else:
@@ -204,6 +181,31 @@ def process_video(vid_location, model_type, processed_vid_location):
             knee_angle = angle_between_points(parts['left_knee'], parts['right_knee'], (0, parts['right_knee'][1])) - 90
         except:
             knee_angle= None
+        
+        try:
+            # Failed attempt at finding the angular movement
+            if (parts['right_shoulder'][0] > parts['left_shoulder'][0]) and (hip_angle < 90):
+                # shoulder_angle = angle_between_points(parts['right_shoulder'], parts['left_shoulder'], (0, parts['left_shoulder'][1])) + 90
+                if parts['left_palm'][0] is not None and (parts['left_palm'][0]<parts['right_shoulder'][0] and parts['left_palm'][1]<parts['right_shoulder'][1]):
+                    print("yes")
+                    shoulder_angle = angle_between_points(parts['left_shoulder'], parts['right_shoulder'], (0, parts['right_shoulder'][1])) - 90
+                elif parts['right_palm'][0] is not None and (parts['right_palm'][0]<parts['right_shoulder'][0] and parts['right_palm'][1]<parts['right_shoulder'][1]):
+                    print("yes_1")
+                    shoulder_angle = angle_between_points(parts['left_shoulder'], parts['right_shoulder'], (0, parts['right_shoulder'][1])) - 90
+                elif parts['left_palm'][0] is None and parts['right_palm'][0] is None:
+                    print("None")
+                    shoulder_angle = None
+                else:
+                    print('latest')
+                    shoulder_angle = angle_between_points(parts['right_shoulder'], parts['left_shoulder'], (0, parts['left_shoulder'][1])) + 90
+            else:
+                print('no')
+                shoulder_angle = angle_between_points(parts['right_shoulder'], parts['left_shoulder'], (0, parts['left_shoulder'][1])) + 90
+        except:
+            print('exp')
+            shoulder_angle = None
+        print(shoulder_angle)
+        
         
         # Using cv2.putText() method 
         try:
@@ -234,10 +236,10 @@ def process_video(vid_location, model_type, processed_vid_location):
         ax.plot(x, y)
         # ax.scatter(parts['right_shoulder'][0], parts['right_shoulder'][1])
         fig.canvas.draw()
-        plt.show()
+        # plt.show()
         image_ = np.array(fig.canvas.renderer.buffer_rgba())
 
-        # cv2.imshow('Left Elbow Angle', image_[:, :, 0:3])
+        cv2.imshow('Left Elbow Angle', image_[:, :, 0:3])
         out.write(image_[:, :, 0:3])
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break #wait until any key is pressed
@@ -245,5 +247,5 @@ def process_video(vid_location, model_type, processed_vid_location):
     out.release()
 
 # process_video(vid_location="input.mp4", model_type="cmu", processed_vid_location="test_out.mp4")
-process_video(vid_location="input.mp4", model_type="mobilenet_thin", processed_vid_location="test_out.mp4")
+process_video(vid_location="input_in.mp4", model_type="cmu", processed_vid_location="test_out.mp4")
 # mobilenet_thin
